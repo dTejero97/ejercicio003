9e83b10e2be982ad0a13698eb9827351dcd64605
dtejero <dtejero@formacion.cic.es>
2020-07-21 12:16:49 +0200

    Añadir interface para calculos

---
+++ ICalculadora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLCalculos
+{
+    interface ICalculadora
+    {
+        public int Casa();
+
+        public int DevolverValor();
+
+    }
+}
